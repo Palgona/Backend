65f03e65044c113e2cbb3305f1788b80f22c3693
hoosa <janghoosa@gmail.com>
2024-01-27 19:31:43 +0900

    Initial commit

---
+++ src/main/java/com/palgona/palgona/PalgonaApplication.java
@@ -0,0 +1,13 @@
+package com.palgona.palgona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalgonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalgonaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/palgona/palgona/PalgonaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palgona.palgona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalgonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
