93ece03871c0a4db58203bf81a11934babdc8663
jinsu868 <mjinsu098@gmail.com>
2024-02-01 00:24:06 +0900

    feat: jwtUtils 클래스 구현
    - jwt 의존성 설정 (build.gradle)
    - JwtUtils 구현
    	- 토큰 파싱, 토큰 생성, 유효성 확인

---
+++ src/main/java/com/palgona/palgona/common/jwt/util/JwtUtils.java
@@ -0,0 +1,63 @@
+package com.palgona.palgona.common.jwt.util;
+
+import io.jsonwebtoken.Jwts;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.Optional;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtils {
+    @Value("${spring.jwt.access.expireMs}")
+    private Long accessExpirationTime;
+
+    @Value("${spring.jwt.refresh.expireMs}")
+    private Long refreshExpirationTime;
+
+    private SecretKey secretKey;
+
+    public JwtUtils(@Value("${spring.jwt.secret}") String secret) {
+        this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8),
+                Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
+    public String createAccessToken(String email) {
+        return Jwts.builder()
+                .claim("email", email)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + accessExpirationTime))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public String createRefreshToken(String email) {
+        return Jwts.builder()
+                .claim("email", email)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + refreshExpirationTime))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public Optional<String> extractEmail(String token) {
+        return Optional.ofNullable(Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .get("email", String.class));
+    }
+
+    public Boolean isExpired(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getExpiration()
+                .before(new Date());
+    }
+}
