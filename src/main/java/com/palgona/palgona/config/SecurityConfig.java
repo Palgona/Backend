52d976fac4b9be91e4fb1be3571a2cff84fbbc0d
jinsu868 <mjinsu098@gmail.com>
2024-01-31 21:53:34 +0900

    chore: Security 설정

---
+++ src/main/java/com/palgona/palgona/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.palgona.palgona.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .formLogin(config -> config.disable())
+                .httpBasic(config -> config.disable())
+                .csrf(config -> config.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/auth/login").permitAll()
+                        .anyRequest().authenticated());
+
+        return http.build();
+    }
+}
