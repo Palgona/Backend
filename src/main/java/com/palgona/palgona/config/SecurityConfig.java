5f3f325f9ddf54abe0b9ca017bc3ef395ce12a91
jinsu868 <mjinsu098@gmail.com>
2024-02-20 00:34:33 +0900

    feat: 인가 설정 및 역할 계층 구조 추가 (#29)

--- src/main/java/com/palgona/palgona/config/SecurityConfig.java
+++ src/main/java/com/palgona/palgona/config/SecurityConfig.java
@@ -8,6 +8,8 @@ import com.palgona.palgona.common.jwt.util.TokenExtractor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -19,6 +21,10 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private static final String ADMIN = "ADMIN";
+    private static final String USER = "USER";
+    private static final String GUEST = "GUEST";
 
     private final JwtUtils jwtUtils;
     private final MemberRepository memberRepository;
@@ -37,11 +43,24 @@ public class SecurityConfig {
                 })
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/login").permitAll()
+                        .requestMatchers("/api/v1/auth/signup").hasRole(GUEST)
                         .requestMatchers("/v3/**", "swagger-ui/**").permitAll()
+                        .requestMatchers("/api/v1/members").hasRole(ADMIN)
+                        .requestMatchers("/api/v1/members/**").hasRole(USER)
+                        .requestMatchers("/api/v1/products/**").hasRole(USER)
+                        .requestMatchers("/api/v1/biddings/**").hasRole(USER)
                         .anyRequest().authenticated())
                 .addFilterBefore(new JwtAuthenticationFilter(jwtUtils, memberRepository, tokenExtractor),
                         UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public RoleHierarchy roleHierarchy() {
+        RoleHierarchyImpl hierarchy = new RoleHierarchyImpl();
+        hierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER > ROLE_GUEST");
+
+        return hierarchy;
     }
 }
