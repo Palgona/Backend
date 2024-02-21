dc9d9f9a718c2d11c8ad56fe0ac86ab012259498
jinsu868 <mjinsu098@gmail.com>
2024-02-21 20:28:42 +0900

    feat: Member, Auth 에러 코드 추가 (#29)

--- src/main/java/com/palgona/palgona/common/error/code/AuthErrorCode.java
+++ src/main/java/com/palgona/palgona/common/error/code/AuthErrorCode.java
@@ -10,7 +10,8 @@ public enum AuthErrorCode implements ErrorCode {
     EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "A_003", "만료된 JWT 토큰입니다."),
     INVALID_SIGNATURE(HttpStatus.UNAUTHORIZED, "A_004", "잘못된 JWT 서명입니다."),
     NOT_SUPPORTED_TOKEN(HttpStatus.UNAUTHORIZED, "A_005", "지원되지 않는 JWT 토큰입니다."),
-    FORBIDDEN_ACCESS(HttpStatus.FORBIDDEN, "A_006", "접근 권한이 없는 리소스입니다.");
+    FORBIDDEN_ACCESS(HttpStatus.FORBIDDEN, "A_006", "접근 권한이 없는 리소스입니다."),
+    ILLEGAL_KAKAO_TOKEN(HttpStatus.UNAUTHORIZED, "A_007", "잘못된 카카오 토큰입니다.");
 
 
     private final HttpStatus status;
