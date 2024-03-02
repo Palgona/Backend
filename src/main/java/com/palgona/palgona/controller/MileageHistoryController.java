fb94b2e00d0f98f13da2433c831a94ff90ef59a6
jangwoojin1 <jangwoojin1@gmail.com>
2024-03-02 18:08:01 +0900

    docs: 마일리지 api 설명 추가 (#23)

--- src/main/java/com/palgona/palgona/controller/MileageHistoryController.java
+++ src/main/java/com/palgona/palgona/controller/MileageHistoryController.java
@@ -3,6 +3,7 @@ package com.palgona.palgona.controller;
 import com.palgona.palgona.common.dto.CustomMemberDetails;
 import com.palgona.palgona.dto.MileageChargeRequest;
 import com.palgona.palgona.service.MileageHistoryService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -15,6 +16,7 @@ public class MileageHistoryController {
     private final MileageHistoryService mileageHistoryService;
 
     @PostMapping
+    @Operation(summary = "마일리지 충전 api", description = "마일리지 충전값을 받아서 충전을 진행한다.")
     public ResponseEntity<Void> chargeMileage(@RequestBody MileageChargeRequest request,@AuthenticationPrincipal CustomMemberDetails memberDetails){
 
         mileageHistoryService.chargeMileage(request, memberDetails);
@@ -23,6 +25,7 @@ public class MileageHistoryController {
     }
 
     @GetMapping
+    @Operation(summary = "마일리지 조회 api", description = "유저의 현재 마일리지 조회를 진행한다.")
     public ResponseEntity<Integer> readMileage(@AuthenticationPrincipal CustomMemberDetails memberDetails){
 
         int mileage = mileageHistoryService.readMileage(memberDetails);
